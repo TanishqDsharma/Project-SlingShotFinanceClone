3b831914860bab78696e2c1f0550e27785d6df83
TanishqDsharma <tanishqsharma612@gmail.com>
2025-05-14 10:52:03 +0530

    Implemented

--- src/interfaces/IWrappedToken.sol
+++ src/interfaces/IWrappedToken.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
+
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+
+interface IWrappedToken is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+}
