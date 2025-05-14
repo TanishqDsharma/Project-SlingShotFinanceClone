4c134b51cfa7a561153ae9f808e49a2c524741e6
TanishqDsharma <tanishqsharma612@gmail.com>
2025-05-14 10:52:13 +0530

    Implemented

--- src/interfaces/ISLingShot.sol
+++ src/interfaces/ISLingShot.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
+
+
+interface ISlingShot{
+    struct TradeFormat{
+        address moduleAddress;
+        bytes encodedCallData;
+    }
+}
