bf7af3fda8eb85005a5a834c0c1a2ce664f27a4f
TanishqDsharma <tanishqsharma612@gmail.com>
2025-05-15 12:15:03 +0530

    UnsiwapModule Implemented

--- src/modules/UniswapModule.sol
+++ src/modules/UniswapModule.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
+
+import "./IUniswapModule.sol";
+
+/// @title Slingshot Uniswap Module
+contract UniswapModule is IUniswapModule {
+
+    // @audit harcoded router address 
+    function getRouter() override public pure returns (address) {
+        return 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    }
+}
