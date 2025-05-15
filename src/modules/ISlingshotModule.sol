8beb06bd67e296235c49ee24619b2b21798fb858
TanishqDsharma <tanishqsharma612@gmail.com>
2025-05-15 07:07:58 +0530

    SlingShot Module

--- src/modules/ISlingshotModule.sol
+++ src/modules/ISlingshotModule.sol
@@ -1,2 +1,11 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
+
+/// @title Slingshot Abstract Module
+/// @dev   the only purpose of this is to allow for easy verification when adding new module
+abstract contract ISlingshotModule {
+    function slingshotPing() public pure returns (bool) {
+        return true;
+    }
+}
+      
